97b021fc3d6499ba4a39e197799c76f342d8e77f
evandoeusebio <evandroivandro65@gmail.com>
2026-09-15 21:03:27 +0100

    feat: added one tools funtion and run this tools in context ollama LLM

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
 from ollama import chat
 from ollama import ChatResponse
+from tools import soma
 
 while True:
         ask = input("Insert questions to ask the model. Type 'exit' to quit: ")
 
-        ask.lower()
-        if (ask == "exit"):
+        if (ask.lower() == "exit"):
                 break
+
+        message = [{'role': 'user', 'content': ask}]
         
-        response: ChatResponse = chat(model='qwen3:4b', messages=[
-        {
-            'role': 'user',
-            'content': ask,
-        },
-        ])
-        # or access fields directly from the response object
-        print(response.message.content)
+        response: ChatResponse = chat(model='qwen3:4b', messages=message, tools=[soma], think=True)
+
+        # append in context assistent decision
+        message.append(response.message.model_dump())
+        
+        if(response.message.tool_calls):
+                print(response.message.tool_calls)
+                call = response.message.tool_calls[0]
+                if call.function.name == "soma":
+                        result = soma(**call.function.arguments)
+
+                        # Append result for context AI
+                        message.append({"role": "tool", "tool_name": call.function.name, "content": str(result)})
+
+                        # Final Response of the all process
+                        final_response: ChatResponse = chat(model='qwen3:4b', messages=message, tools=[soma], think=True)
+
+
+                        print(final_response.message.content)
---
+++ src/tools/calculator.py
@@ -0,0 +1,14 @@
+def soma( a: int, b: int) -> int:
+
+    """
+    Function that sums two integers.
+    
+    Args:
+        a (int): The first integer to sum.
+        b (int): The second integer to sum.
+    
+    Returns:
+        int: The sum of the two integers.
+    
+    """
+    return a + b
